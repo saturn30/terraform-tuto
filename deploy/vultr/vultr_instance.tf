a0d3aeed37efd45cf90c58389e76285bebe72eb0
saturn30 <soso01dev@gmail.com>
2024-07-20 01:29:52 +0900

    블록스토리지 연결

--- deploy/vultr/vultr_instance.tf
+++ deploy/vultr/vultr_instance.tf
@@ -3,3 +3,9 @@ resource "vultr_instance" "example_instance" {
   region = "lax"
   os_id = "387"
 }
+
+resource "vultr_block_storage" "example_blockstorage" {
+  size_gb = 10
+  region = "lax"
+  attached_to_instance = "${vultr_instance.example_instance.id}"
+}
