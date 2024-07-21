2a0d55265cb0d91671d6ebb7b339417335a89479
saturn30 <soso01dev@gmail.com>
2024-07-21 12:04:46 +0900

    null_resource로 쿠버네티스 접속 보장

--- deploy/vke/main.tf
+++ deploy/vke/main.tf
@@ -14,6 +14,20 @@ resource "vultr_kubernetes" "test_kube" {
   }
 }
 
+resource "null_resource" "wait_for_cluster" {
+  depends_on = [local_file.kube_config]  // kubeconfig 파일 생성 후 실행
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      until kubectl --kubeconfig=${local_file.kube_config.filename} get nodes &>/dev/null; do
+        echo "클러스터가 아직 준비되지 않았습니다. 10초 후 다시 시도합니다..."
+        sleep 10
+      done
+      echo "Kubernetes 클러스터 연결 성공"
+    EOT
+  }
+}
+
 resource "helm_release" "cert_manager" {
   name       = "cert-manager"
   repository = "https://charts.jetstack.io"
@@ -25,7 +39,7 @@ resource "helm_release" "cert_manager" {
     name  = "installCRDs"
     value = "true"
   }
-  depends_on = [ local_file.kube_config ]
+  depends_on = [ null_resource.wait_for_cluster ]
 }
 
 resource "helm_release" "nginx_ingress" {
@@ -34,5 +48,5 @@ resource "helm_release" "nginx_ingress" {
   chart      = "ingress-nginx"
   namespace  = "ingress-nginx"
   create_namespace = true
-  depends_on = [ local_file.kube_config ]
+  depends_on = [ null_resource.wait_for_cluster ]
 }
